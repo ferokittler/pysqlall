60b9b897bcaef422c117425aabbdf67de92d6d32
ferokittler <kittler.fero@gmail.com>
2014-05-28 10:15:38 +0200

    Add support for parsing files for configurationParser

--- pysqlall/configurationParser.py
+++ pysqlall/configurationParser.py
@@ -2,6 +2,9 @@
 # database_type;username;password;connection_string:script
 # oracle;hr;hr;hrdb.example.com;script;
 
+#from pysqlall import database
+import database
+
 
 class ConfigurationParser(object):
     """
@@ -12,6 +15,8 @@ class ConfigurationParser(object):
     INLINE = 2
 
     def __init__(self, configuration = ""):
+    
+        self.databases = []
 
         if configuration.strip() == "":
             raise ValueError("Configuration is empty!")
@@ -25,31 +30,38 @@ class ConfigurationParser(object):
 
     def parse(self):
         if self.configurationType == self.INLINE:
-            self.doParsing()
+            self.doParsing(self.configuration.split('\n'))
 
         if self.configurationType == self.FILE:
             try:
                 handle = open(self.configuration[1:])
-                close(handle)
+                self.doParsing(handle.readlines()) 
+                handle.close()
             except IOError:
                 raise ValueError('Configuration file %s does not exists!' % self.configuration[1:])
             
             
 
 
-    def doParsing(self):
+    def doParsing(self, configurationContent):
         """
             Parse configuration file (or configuration passed on command line?)
             to fill configuration object with values
         """
         
-        lines = self.configuration.split('\n')
+        #lines = self.configurationContent.split('\n')
         i = 1
-        for line in lines:
-            if line.count(';') > 5:
+        for line in configurationContent:
+            line = line.strip()
+            if line.count(';') > 4:
                 raise ValueError("Configuration on line %d has incorrect format - too many semicolons!" % i)
 
-            if line.count(';') < 5:
+            if line.count(';') < 4:
                 raise ValueError("Configuration on line %d has incorrect format - too few semicolons!" % i)
+
+            type, username, password, connection, script = line.split(';')
+            self.databases.append(database.Database(type, username, password, connection, script))
             i+=1
+
             
+
--- tests/configurationParserTest.py
+++ tests/configurationParserTest.py
@@ -1,5 +1,7 @@
 import unittest
 from pysqlall import configurationParser
+from pysqlall import database
+import os
 
 class TestConfigurationParser(unittest.TestCase):
 
@@ -19,33 +21,75 @@ class TestConfigurationParser(unittest.TestCase):
         self.assertRaisesRegexp(ValueError, "Configuration is empty!", configurationParser.ConfigurationParser, " \n  \t  ")
 
     def testCreationOfConfigurationParserWithInlineConfiguration(self):
-        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script;")
-        self.assertEquals(self.cp.configuration, "oracle;hr;hr;hrdb.example.com;script;")
+        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script")
+        self.assertEquals(self.cp.configuration, "oracle;hr;hr;hrdb.example.com;script")
 
     def testFileConfigurationType(self):
         self.cp = configurationParser.ConfigurationParser("@/tmp/nonexistent.sql")
         self.assertEquals(self.cp.configurationType, self.cp.FILE)
 
     def testInlineConfigurationType(self):
-        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script;")
+        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script")
         self.assertEquals(self.cp.configurationType, self.cp.INLINE)
 
-    def testParseInlineConfigurationWith6semicolons(self):
-        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script;;")
+    def testParseInlineConfigurationWith5semicolons(self):
+        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script;")
         self.assertRaisesRegexp(ValueError, "Configuration on line 1 has incorrect format - too many semicolons", self.cp.parse)
 
-    def testParseInlineConfigurationWith5semicolons(self):
-        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script")
+    def testParseInlineConfigurationWith3semicolons(self):
+        self.cp = configurationParser.ConfigurationParser("oraclehr;hr;hrdb.example.com;script")
         self.assertRaisesRegexp(ValueError, "Configuration on line 1 has incorrect format - too few semicolons", self.cp.parse)
 
-    def testParseInlineConfigurationWith5semicolonsOnSecondLine(self):
-        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script;\noracle;hr;hr;hrdb2.example.com;script")
+    def testParseInlineConfigurationWith3semicolonsOnSecondLine(self):
+        self.cp = configurationParser.ConfigurationParser("oracle;hr;hr;hrdb.example.com;script\noracle;hrhr;hrdb2.example.com;script")
         self.assertRaisesRegexp(ValueError, "Configuration on line 2 has incorrect format - too few semicolons", self.cp.parse)
 
     def testFileConfigurationWhenFileDoesNotExists(self):
         self.cp = configurationParser.ConfigurationParser("@/tmp/nonexistent.sql")
         self.assertRaisesRegexp(ValueError, "Configuration file /tmp/nonexistent.sql does not exists!", self.cp.parse)
+
+    def testConfigurationParserWithSimpleConfigurationBeforeParsing(self):
+        self.cp = configurationParser.ConfigurationParser("oracle;hr;hrpwd;hrdb.example.com;script")
+        self.assertListEqual(self.cp.databases, [])
+
+    def testConfigurationParserWithCorrectSingleLineConfiguration(self):
+        self.cp = configurationParser.ConfigurationParser("oracle;hr;hrpwd;hrdb.example.com;script")
+        self.cp.parse()
+        self.d = database.Database("oracle","hr","hrpwd","hrdb.example.com","script")
+        self.assertListEqual(self.cp.databases, [self.d])
+
+    def testConfigurationParserWithCorrectMultipleLineConfiguration(self):
+        self.cp = configurationParser.ConfigurationParser("oracle;hr;hrpwd;hrdb.example.com;script\noracle;hr2;hrpwd2;hrdb2.example.com;script2")
+        self.cp.parse()
+        self.d1 = database.Database("oracle","hr","hrpwd","hrdb.example.com","script")
+        self.d2 = database.Database("oracle","hr2","hrpwd2","hrdb2.example.com","script2")
+        self.assertListEqual(self.cp.databases, [self.d1, self.d2])
         
+
+    def testFileConfigurationParserWithCorrectSingleLineConfiguration(self):
+        filename = '/tmp/test1.txt'
+        self.f = open(filename, 'w')
+        self.f.write("oracle;hr;hrpwd;hrdb.example.com;script\n")
+        self.f.close()
+
+        self.cp = configurationParser.ConfigurationParser('@' + filename)
+        self.cp.parse()
+        self.d = database.Database("oracle","hr","hrpwd","hrdb.example.com","script")
+        self.assertListEqual(self.cp.databases, [self.d])
+        os.remove(filename)
+
+    def testFileConfigurationParserWithCorrectMultipleLineConfiguration(self):
+        filename = '/tmp/test2.txt'
+        self.f = open(filename, 'w')
+        self.f.write("oracle;hr;hrpwd;hrdb.example.com;script\noracle;hr2;hrpwd2;hrdb2.example.com;script2")
+        self.f.close()
+
+        self.cp = configurationParser.ConfigurationParser('@' + filename)
+        self.cp.parse()
+        self.d1 = database.Database("oracle","hr","hrpwd","hrdb.example.com","script")
+        self.d2 = database.Database("oracle","hr2","hrpwd2","hrdb2.example.com","script2")
+        self.assertListEqual(self.cp.databases, [self.d1, self.d2])
+        os.remove(filename)
 
 
 
