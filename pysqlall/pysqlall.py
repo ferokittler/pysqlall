193dd66d020c239cb9105ff43c0139c8d49bb2d4
ferokittler <kittler.fero@gmail.com>
2014-06-02 10:20:22 +0200

    Add class for main object

--- pysqlall/pysqlall.py
+++ pysqlall/pysqlall.py
@@ -1,10 +1,17 @@
 import argparse
+import configuration
 
 # TODO: object design
 
 # Config file example
 # database_type;username;password;connection_string:script
 # oracle;hr;hr;hrdb.example.com;script;
+
+class Pysqlall(object):
+    def __init__(self, args):
+        self.configuration = configuration.Configuration(args.database, args.username, args.password, args.script)
+        #self.configuration.read()
+
 
 def createParser():
     parser = argparse.ArgumentParser(description = "Tool for running SQL code on multiple databases")
@@ -19,8 +26,12 @@ def main():
     parser = createParser()
     args = parser.parse_args()
 
+    pysqlall = Pysqlall(args)
+    
+
     # Do whatever you need to do
     print "Here comes the sun..."
+
 
 if __name__ == '__main__':
     main()
